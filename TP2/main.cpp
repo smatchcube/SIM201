94a029c8603bc75111bbcd93cdfec11d42235b9c
smatchcube <smatchcube@tfwno.gf>
2020-11-19 17:22:37 +0100

    change

--- TP2/main.cpp
+++ TP2/main.cpp
@@ -5,7 +5,12 @@
 
 
 int main() {
-    matrice H = hilbert(4);
-    H.print();
+    matrice A = hilbert(5);
+    vecteur B;
+    B.init(5, 2);
+    double eps = 10e-6; // a changer plus tard pour une valeur adaptee
+    vecteur X = gauss(A, B, eps);
+    vecteur Y = produit(A, X);
+    Y.print();
     return EXIT_SUCCESS;
 }
--- TP2/vecteur_matrice.cpp
+++ TP2/vecteur_matrice.cpp
@@ -115,4 +115,58 @@ vecteur cl(const vecteur& X, double a, const vecteur& Y, double b) {
     return Z;
 }
 
-vecteur gauss(const matrice& A, const vecteur& B, double eps);
+vecteur gauss(const matrice& A, const vecteur& B, double eps) {
+    matrice M;
+    vecteur Y;
+    
+    M.init(A);
+    Y.init(B);
+    
+    size_t r = 0;
+    size_t c = 0;
+    size_t p;
+    double v;
+    
+    while (r < M.dim && c < M.dim) {
+    	p = r;
+    	v = M.lignes[p][c];
+    	for (size_t i = r; i < M.dim; ++i) {
+    	     if (abs(M.lignes[i][r]) > v) {
+    	         p = i;
+    	         v = abs(M.lignes[i][r]);
+    	     }
+    	}
+        if (abs(M.lignes[p][c]) < eps)
+            ++c;
+        else {
+            // on echange les colonnes de M
+            for (size_t j = 0; j < M.dim; ++j) {
+                v = M.lignes[r][j];
+                M.lignes[r][j] = M.lignes[p][j];
+                M.lignes[p][j] = v;
+            }
+            // on echange aussi dans le vecteur colonne Y
+            v = Y.val[r];
+            Y.val[r] = Y.val[p];
+            Y.val[p] = v;
+            
+            // On elimine sur les lignes en dessous du pivot
+            for (size_t i = r + 1; i < M.dim; ++i) {
+                v = M.lignes[i][c] / M.lignes[r][c];
+                M.lignes[i][c] = 0;
+                for (size_t j = c + 1; j < M.dim; ++j)
+                    M.lignes[i][j] -= v * M.lignes[r][j];
+                Y.val[i] -= v * Y.val[r];
+            }
+            ++r;
+            ++c;
+        }
+    }
+    return Y;
+}
+
+
+
+
+
+
